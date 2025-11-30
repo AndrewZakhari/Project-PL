55581c550012e70f7a8ba96fdab87f926c57780a
Andrew Zakhari <andrewzakhari5@gmail.com>
2025-11-30 20:05:51 +0200

    Implemented File handling

--- src/main/java/com/inventory/service/FileService.java
+++ src/main/java/com/inventory/service/FileService.java
@@ -1,5 +1,55 @@
 package com.inventory.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileService {
-    // Helper methods for reading/writing files
+  private String DIR = "../data";
+  
+  public List<String> readAllLines(String fileName) {
+    try {
+     java.nio.file.Path path = Paths.get(DIR + fileName);
+     
+     if(Files.exists(path)){
+       return Files.readAllLines(path);
+     }else {
+       reutrn new ArrayList<>();
+     }
+    } catch (Exception e) {
+      e.printStackTrace();
+      return new ArrayList<>();
+    }
+  }
+
+  public void appendToFile(String fileName, String data){
+    try {
+      java.nio.file.Path path = Paths.get( DIR + filename);
+
+      String dataWithNewLine = data + System.lineSeperator();
+      
+      // StandardOpenOption.CREATE here creates the file if it's missing APPEND appends to the end of the file
+      Files.write(path, dataWithNewLine.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+      
+    } catch (Exception e) {
+     System.err.println("Error writing to file: ", fileName) ;
+     e.printStackTrace();
+    }
+  }
+  public void overwriteFile(String fileName, List<String> data) {
+    try {
+      java.nio.file.Path path = Paths.get( DIR + fileName);
+
+      Files.write(path, data);
+
+    } catch (IOException e) {
+      System.err.println("Error overwriting file: " + fileName );
+      e.printStackTrace();
+    }
+  }
 }
+
+
