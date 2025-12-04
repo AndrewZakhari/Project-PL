6985da9f6e4cf96a2a3587a7cc088e429b451487
MAHRAN-JJ <janamahran7@gmail.com>
2025-12-04 16:11:08 +0200

    i've done model category

--- src/main/java/com/inventory/model/Offer.java
+++ src/main/java/com/inventory/model/Offer.java
@@ -1,12 +1,57 @@
 package com.inventory.model;
 
 import java.time.LocalDate;
+import com.inventory.model.Product;
 
 public class Offer {
     private Product product;
     private double discountPercentage;
     private LocalDate startDate;
     private LocalDate endDate;
+
+    //constructor
+    public Offer(Product product,double discountPercentage,LocalDate startDate,LocalDate EndDate){
+        this.product = product;
+        this.discountPercentage = discountPercentage;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+    //getters
+    public Product getproduct(){
+        return product;
+    }
+    public double discountPercentage() {
+        return discountPercentage;
+    }
+    public LocalDate getstartDate() {
+        return startDate;
+    }
+    public LocalDate getEndDate(){
+        return endDate;
+    }
+    //setters
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    public void setDiscountPercentage(double discountPercentage) {
+        this.product = product;
+    }
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+    public void setEndDate(LocalDate endDate){
+        this.endDate = endDate;
+    }
+    // toString method for saving in file
+    @Override
+    public String toString(){
+        return product.getName() + "," + discountPercentage + "," + startDate + "," + endDate;
+
+    }
+
+
+
+
 
     // Getters and Setters
 }
--- src/main/java/com/inventory/model/Supplier.java
+++ src/main/java/com/inventory/model/Supplier.java
@@ -1,8 +1,41 @@
 package com.inventory.model;
-
+//getters and setters
 public class Supplier {
     private String name;
     private String contactInfo;
+    //constructor
+    public Supplier(String name,String contactInfo){
+        this.name=name;
+        this.contactInfo=contactInfo;
+    }
+    //getter for name
+    public String getName(){
+        return name;
+    }
+    //setter for name
+    public void setName(String name){
+        this.name=name;
+    }
+    //getter for contactinfo
+    public String getContactInfo(){
+        return contactInfo;
+    }
+    //setter for contact info
+    public void setContactInfo(String contactInfo){
+        this.contactInfo=contactInfo;
+    }
+    //to string for saving in file
+    @Override
+    public String toString(){
+        return name + "," + contactInfo;
+    }
 
-    // Getters and Setters
+    public static Supplier fromCsv(String line){
+        String[] parts=line.split(",");
+        return new Supplier(parts[0], parts.length > 1 ? parts[1] : "");
+    }
+
+
+
+
 }
